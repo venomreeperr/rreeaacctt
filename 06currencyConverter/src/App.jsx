e595f429716e12960b37860a62391bf9fdf31771
venomreeperr <snehsharmacollegematerial@gmail.com>
2026-02-02 04:04:30 +0530

    bhaiiiiiii

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -27,7 +27,8 @@ function App() {
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
         style={{
             backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
-        }}>
+        }}
+>
         <div className="w-full">
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
         <form
