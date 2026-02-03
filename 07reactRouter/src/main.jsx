4cc0866aaac3f6dfc376359fd4d789659e58b89b
venomreeperr <snehsharmacollegematerial@gmail.com>
2026-02-03 15:51:35 +0530

    Integrate React Router for navigation and replace App component with RouterProvider

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './components/Layout/Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
 
-createRoot(document.getElementById('root')).render(
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "", 
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+
+      }
+    ],
+  },
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
