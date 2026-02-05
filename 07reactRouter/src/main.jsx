b0a58b4b0f49d098e89f6f0d86ef423f9206f56d
venomreeperr <snehsharmacollegematerial@gmail.com>
2026-02-05 15:26:01 +0530

    feat: Add React Router setup with routes for home, about, contact, and dynamic user pages.

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,37 +2,48 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './components/Layout/Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/contact.jsx'
-
-
-
-
-
-const router = createBrowserRouter([
-  {
-    path: '/',
+import User from './components/User/User.jsx'
+//const router = createBrowserRouter([
+{
+  path: '/',
     element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />
-      },
-      {
-        path: "about",
-        element: <About />
+      children: [
+        {
+          path: "",
+          element: <Home />
+        },
+        {
+          path: "about",
+          element: <About />
 
-      },
-      {
-        path: "contact",
-        element: <Contact />
-      }
-    ],
+        },
+        {
+          path: "contact",
+          element: <Contact />
+        }
+      ],
   },
-])
+//])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+    </Route>
+  )
+
+)
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
