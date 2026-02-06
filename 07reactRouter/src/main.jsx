f7897ed878db615acfd9bb6237a20fda2594e933
venomreeperr <snehsharmacollegematerial@gmail.com>
2026-02-06 15:19:50 +0530

    feat: Set up React Router with nested routes, dynamic user IDs, and a GitHub profile page with data loading.

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -8,26 +8,26 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/contact.jsx'
 import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 //const router = createBrowserRouter([
-{
-  path: '/',
-    element: <Layout />,
-      children: [
-        {
-          path: "",
-          element: <Home />
-        },
-        {
-          path: "about",
-          element: <About />
-
-        },
-        {
-          path: "contact",
-          element: <Contact />
-        }
-      ],
-  },
+//      {
+//      path: '/',
+//      element: <Layout />,
+//      children: [
+//        {
+//          path: "",
+//          element: <Home />
+//        },
+//        {
+//          path: "about",
+//          element: <About />
+//        },
+//        {
+//          path: "contact",
+//          element: <Contact />
+//        }
+//      ],
+//    },
 //])
 
 const router = createBrowserRouter(
@@ -37,6 +37,11 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
+      <Route
+        loader={githubInfoLoader}
+        path='github'
+        element={<Github />}
+      />
     </Route>
   )
 
