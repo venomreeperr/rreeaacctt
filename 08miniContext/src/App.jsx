718c4b3e9649ca8383375a98e512fe9dcf275b8f
venomreeperr <snehsharmacollegematerial@gmail.com>
2026-02-08 16:32:38 +0530

    feat: Implement basic user authentication and state management using React Context API.

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>Context API</h1>
+    <UserContextProvider>
+      <h1>Context API</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
