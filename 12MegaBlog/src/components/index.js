8fb02c3d69eda00135b167d50f332f25f1da8efa
venomreeperr <snehsharmacollegematerial@gmail.com>
2026-08-05 17:05:50 +0530

    kjb

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,27 +1,30 @@
-import Header from "./Header/Header"
-import Footer from "./Footer/Footer"
-import Logo from "./Logo"
-import Container from "./container/Container"
-import LogoutBtn from "./Header/LogoutBtn"
-import RTE from "./RTE"
-import PostCard from "./PostCard"
-import Login from "./Login"
-import Signup from "./Signup"
-import PostForm from "./PostForm"
-
-
-
-
+import Select from "./Select";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
 export {
     Header,
     Footer,
     Container,
+    Logo,
     LogoutBtn,
     RTE,
-    PostCard,
-    Login,
     Signup,
+    Login,
+    Button,
     PostForm,
-    Logo
-
+    PostCard,
+    AuthLayout,
+    Input,
+    Select,
 }
